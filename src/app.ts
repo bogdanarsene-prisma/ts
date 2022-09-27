0eab811693cfb70fb302d4cb5fc90dba7d94f4fd
Bogdan Arsene <arsene@prisma-analytics.com>
2022-09-27 11:04:38 +0300

    pdf form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
 console.log('hi.');
+
+import {PDFDocument} from 'pdf-lib';
+import * as fs from 'fs';
+
+async function fillForm() {
+  const formFile = 'D112_completata.pdf';
+  const uint8Array = fs.readFileSync(formFile);
+  const pdfDoc = await PDFDocument.load(uint8Array);
+
+  const form = pdfDoc.getForm();
+
+  const asd = form.acroForm;
+
+  const pdfBytes = await pdfDoc.save();
+}
+
+fillForm();
