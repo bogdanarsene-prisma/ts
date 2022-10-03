81a462cd051f2a4110ee63a841fde1b5749415b2
Bogdan Arsene <arsene@prisma-analytics.com>
2022-10-03 23:23:19 +0300

    first

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 console.log('hi.');
+
+const seedrandom = require('seedrandom');
+const crypto = require('crypto');
+
+const rng = seedrandom('hello.');
+console.log(rng()); // Always 0.9282578795792454
+
+function random(min: number, max: number): number {
+  return rng() * (max - min) + min;
+}
+
+function randomInt(min: number, max: number): number {
+  return Math.round(random(min, max));
+}
+
+function hash(string: string) {
+  const hex = crypto.createHash('sha256').update(string).digest('hex');
+  const int = parseInt(hex, 16);
+  const base = 2 ** 256;
+  const float = int / base;
+  return float;
+}
+
+console.log(hash('bogdan'));
