26d4675fd78017be31ef6f0eb127555bda8ad073
Bogdan Arsene <arsene@prisma-analytics.com>
2022-10-05 17:02:27 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,15 @@ console.log('hi.');
 const seedrandom = require('seedrandom');
 const crypto = require('crypto');
 
-const rng = seedrandom('hello.');
-console.log(rng()); // Always 0.9282578795792454
+import crc from 'crc';
 
-function random(min: number, max: number): number {
-  return rng() * (max - min) + min;
+function random(string: string): number {
+  const rng = seedrandom(string);
+  const float = rng();
+  return float;
 }
 
-function randomInt(min: number, max: number): number {
-  return Math.round(random(min, max));
-}
-
-function hash(string: string) {
+function hash_sha(string: string) {
   const hex = crypto.createHash('sha256').update(string).digest('hex');
   const int = parseInt(hex, 16);
   const base = 2 ** 256;
@@ -22,4 +19,51 @@ function hash(string: string) {
   return float;
 }
 
-console.log(hash('bogdan'));
+function hash_crc(string: string) {
+  const int = crc.crc32(string);
+  const base = 2 ** 32;
+  const float = int / base;
+  return float;
+}
+
+function getInt(
+  string: string,
+  min: number,
+  max: number,
+  callback: Function
+): number {
+  const hash = callback(string);
+  return Math.round(hash * (max - min) + min);
+}
+
+function getSeries(
+  string: string,
+  length: number,
+  min: number,
+  max: number,
+  callback: Function
+): number[] {
+  const series: number[] = [];
+
+  while (length > 0) {
+    const hash = callback(string);
+    const value = Math.round(hash * (max - min) + min);
+    series.push(value);
+  }
+
+  return series;
+}
+
+console.log('==================');
+console.log(random('bogdan'));
+console.log(random('bogdan1'));
+console.log(random('bogdan2'));
+console.log('==================');
+console.log(hash_sha('bogdan'));
+console.log(hash_sha('bogdan1'));
+console.log(hash_sha('bogdan2'));
+console.log('==================');
+console.log(hash_crc('bogdan'));
+console.log(hash_crc('bogdan1'));
+console.log(hash_crc('bogdan2'));
+console.log('==================');
